707249c2f7bde1a9f8f102fea757611b5368b359
ceen <ngyn.christopher@gmail.com>
2021-09-09 17:56:37 -0700

    added contact page.  It doesn't work yet though lmao

--- src/components/main-portfolio/contact/Contact.jsx
+++ src/components/main-portfolio/contact/Contact.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import "./contact.scss"
+import React, { useState } from "react";
+import "./contact.scss";
 
-export default function contact() {
+export default function Contact() {
+    const [thankYouMessage, setThankYouMessage] = useState(false);
+    const submitHandler = (e) => {
+        setThankYouMessage(true);
+        e.preventDefault();
+    };
     return (
         <div id="contact-me" className="contact">
-            Contact me!
+            <div className="section section__left">
+                <div className="section__left--item img--container">
+                    <img
+                        className="left__item--img"
+                        src="img/glass_bottle_in_water_pngtree_trans.png"
+                        alt="mail"
+                    ></img>
+                </div>
+            </div>
+            <div className="section section__right">
+                {!thankYouMessage ? (
+                    <div className="section__form">
+                        <h2 className="right__item right__item--title">
+                            Contact.
+                        </h2>
+                        <form
+                            onSubmit={submitHandler}
+                            className="right__item right__item--form"
+                        >
+                            <input type="text" placeholder="email@email.com" />
+                            <textarea
+                                placeholder="Message"
+                                cols="30"
+                                rows="10"
+                            ></textarea>
+                            <button className="form--submit">Send</button>
+                        </form>
+                    </div>
+                ) : (
+                    <div className="thank-you-msg">
+                        <span className="thank-you-msg--text">Thanks for the message! I'll get back to you soon...</span>
+                    </div>
+                )}
+            </div>
         </div>
-    )
+    );
 }
